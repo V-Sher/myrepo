f50ace2ad52116dccd106b4137bcf1c5e8c86904
Varshita Sher <varshitasher@gmail.com>
2019-08-04 11:52:35 -0700

    Adding the comments for saving workspaces after each modification to variable

--- Example1.R
+++ Example1.R
@@ -9,5 +9,10 @@ y <- y*2
 x <- 10
 y <- 20
 
+#save.image("~/Desktop/desk/myrepo/workspace 1 with 5 elements.RData")
+
 #closing for the day
 boo <- 45
+boo2 <- 15*3
+
+#save.image("~/Desktop/desk/myrepo/workspace 1 with 5 elements.RData")
